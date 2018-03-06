3780137ffb6be5eb1966c55088e8f49a2bd50bde
Bitiquinho <consoni_2519@hotmail.com>
2018-03-06 14:59:26 -0300

    Add Doxygen documentation

---
+++ docs/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['simple_2ddata_2dlogging',['Simple-Data-Logging',['../index.html',1,'']]]
+];
---
+++ docs/search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['data_5flog_5fmax_5fprecision',['DATA_LOG_MAX_PRECISION',['../data__logging_8h.html#a7cdfa76a6103adba39299087b4bfc441',1,'data_logging.h']]]
+];
